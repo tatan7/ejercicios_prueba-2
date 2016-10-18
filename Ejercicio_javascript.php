848653590c91ff95c0a70d2b83fd446a048a3965
tatan <jonlomed7@hotmail.com>
2016-10-18 12:58:47 -0500

    se sube el ejercicio propuesto con los cambios de javascript

--- Ejercicio_javascript.php
+++ Ejercicio_javascript.php
@@ -6,14 +6,23 @@
 		<title>convertidor</title>
 		</head>
 		<body bgcolor = "#008EF4">
-		
+		<form name = "convertidor">
+		<h2> CONTADOR <br> DE BILLETES</h2>
+		<br> CUANTOS BILLETES TENGO: <input type="text" name="pesos">
+		<hr>
+		<br>
+		<br> <input type = "button" value = "CONVERTIR" onclick="calcular ();">
+		</form>
+		<scrip type="text/javascript">
+		<script type="text/javascript">
+			function calcular (){
+			var pes = (document.convertidor.pesos.value)*1;
+			if (pes == 0) {alert ("No ha llenado el espacio de pesos ingrese un numero por favor ")}
+			var tot = pes / 16; 
+			tot = tot.totfixed (2);
+			document .convertidor.dolar.value = tot;
+		}		
+		</script>
+		<body>
 
-
-		
-	
-
-
-	<body>
-            
-<?php 
-
+         <?php 
